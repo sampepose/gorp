a5b335eadae5eb0537105c0a5b6f0b24e05565a7
Quinn Slack <qslack@qslack.com>
2013-05-22 16:08:28 -0700

    Use DB-agnostic bind var

--- gorp_test.go
+++ gorp_test.go
@@ -435,7 +435,8 @@ func TestHooks(t *testing.T) {
 	}
 
 	var persons []*Person
-	rawselect(dbmap, &persons, "select * from person_test where id = ?", p1.Id)
+	bindVar := dbmap.Dialect.BindVar(0)
+	_rawselect(dbmap, &persons, "select * from person_test where id = "+bindVar, p1.Id)
 	if persons[0].LName != "postget" {
 		t.Errorf("p1.PostGet() didn't run after select: %v", p1)
 	}
